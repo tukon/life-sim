27e3e36598a262f428479c65afc60010f180ecb8
tukon <adam.armbrester+github@gmail.com>
2017-01-18 18:59:13 -0600

    Create NetBeans project

---
+++ src/edu/cs499/LifeSim.java
@@ -0,0 +1,22 @@
+/* 
+ * Project: Life Sim for CS 499
+ * Team 2: Adam, Alicia, Matt, Paul
+ */
+package edu.cs499;
+
+/**
+ * The main class for the program.
+ * @author adam
+ */
+public class LifeSim {
+
+	/**
+	 * The starting point for the program.
+	 * @param args Command line arguments.
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+	
+}
+// EOF
