677d48b1e6f4d36f65629b781c355f174d603be0
tukon <adam.armbrester+github@gmail.com>
2017-01-23 17:49:30 -0600

    Use JavaFX

--- src/edu/cs499/LifeSim.java
+++ src/edu/cs499/LifeSim.java
@@ -4,19 +4,47 @@
  */
 package edu.cs499;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 /**
- * The main class for the program.
+ * The main application class.
  * @author adam
  */
-public class LifeSim {
+public class LifeSim extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) {
+		Button btn = new Button();
+		btn.setText("Say 'Hello World'");
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Hello World!");
+			}
+		});
+		
+		StackPane root = new StackPane();
+		root.getChildren().add(btn);
+		
+		Scene scene = new Scene(root, 300, 250);
+		
+		primaryStage.setTitle("Hello World!");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 
 	/**
-	 * The starting point for the program.
-	 * @param args Command line arguments.
+	 * @param args The command line arguments.
 	 */
 	public static void main(String[] args) {
-		// TODO code application logic here
+		launch(args);
 	}
 	
 }
-// EOF
